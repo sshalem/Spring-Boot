41032eda8ac96b90a7d11f0ee9544bb6b2a99135
sshalem <shabtay.shalem@gmail.com>
2022-01-02 20:25:59 +0200

    update 02-01-2022

---
+++ 3. Exception Handling/_1_handle_RuntimeExcpetion/code/Handle-Exception/src/main/java/com/app/controller/CustomerController.java
@@ -0,0 +1,22 @@
+package com.app.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.app.service.CustomerService;
+
+@RestController
+@RequestMapping("/customer")
+public class CustomerController {
+
+	@Autowired
+	private CustomerService customerService;
+
+	@GetMapping("/{name}")
+	public String getName(@PathVariable("name") String name) {
+		return customerService.testMethod(name);
+	}
+}
---
+++ 3. Exception Handling/_1_handle_RuntimeExcpetion/code/Handle-Exception/src/main/java/com/app/service/CustomerService.java
@@ -0,0 +1,17 @@
+package com.app.service;
+
+import org.springframework.stereotype.Component;
+
+import com.app.exception.NameAlreadyExistException;
+
+@Component
+public class CustomerService {
+
+	private final String NAME = "karin";
+
+	public String testMethod(String name) {
+		if (name.equals(this.NAME))
+			throw new NameAlreadyExistException("Name " + this.NAME + " already exist");
+		return name;
+	}
+}
