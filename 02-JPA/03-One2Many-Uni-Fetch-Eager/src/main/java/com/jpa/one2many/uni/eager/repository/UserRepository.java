34d602de44b3d1c397dfd049188cd567bffed336
sshalem <shabtay.shalem@gmail.com>
2022-11-01 04:56:43 +0200

    01-11-22 04:56

--- 02-JPA/03-One2Many-Uni-Fetch-Eager/src/main/java/com/jpa/one2many/uni/eager/repository/UserRepository.java
+++ 02-JPA/03-One2Many-Uni-Fetch-Eager/src/main/java/com/jpa/one2many/uni/eager/repository/UserRepository.java
@@ -7,7 +7,6 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import com.jpa.one2many.uni.eager.entity.RoleEntity;
 import com.jpa.one2many.uni.eager.entity.UserEntity;
 
 @Repository
