6e101d02503d1e0610ecf42ecb46b55f061216d4
Shabtay Shalem <shabtay.shalem@gmail.com>
2022-11-08 10:34:59 +0200

    08-11-22 10:34

--- 02-JPA/02-One2Many-Bi-Fetch-Lazy/src/main/resources/data.sql
+++ 02-JPA/02-One2Many-Bi-Fetch-Lazy/src/main/resources/data.sql
@@ -2,16 +2,16 @@
 -- Set FOREIGN KEY CHECK to 0 
 -- TRUNCATE the data from tables
 ----------------------------------
-SET FOREIGN_KEY_CHECKS=0; 
-TRUNCATE TABLE roles_tb;
-TRUNCATE TABLE users_tb;
-TRUNCATE TABLE tutorial_tags;
-TRUNCATE TABLE tutorials;
-TRUNCATE TABLE tags;
-TRUNCATE TABLE course_student;
-TRUNCATE TABLE student_tb;
-TRUNCATE TABLE course_tb;
-SET FOREIGN_KEY_CHECKS=1;
+-- SET FOREIGN_KEY_CHECKS=0; 
+-- TRUNCATE TABLE roles_tb;
+-- TRUNCATE TABLE users_tb;
+-- TRUNCATE TABLE tutorial_tags;
+-- TRUNCATE TABLE tutorials;
+-- TRUNCATE TABLE tags;
+-- TRUNCATE TABLE course_student;
+-- TRUNCATE TABLE student_tb;
+-- TRUNCATE TABLE course_tb;
+-- SET FOREIGN_KEY_CHECKS=1;
 
 -----------------------------------
 -- DROP TABLEs if exist
