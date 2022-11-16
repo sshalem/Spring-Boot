3b52abccafdd0a7faee26e2bb5bfe10db24332e4
sshalem <shabtay.shalem@gmail.com>
2022-11-16 10:08:03 +0200

    16-11-2022 10:08

--- 02-JPA/spring-boot-many-to-many-master/src/main/java/com/bezkoder/spring/hibernate/manytomany/exception/ErrorMessage.java
+++ 02-JPA/spring-boot-many-to-many-master/src/main/java/com/bezkoder/spring/hibernate/manytomany/exception/ErrorMessage.java
@@ -3,31 +3,75 @@ package com.bezkoder.spring.hibernate.manytomany.exception;
 import java.util.Date;
 
 public class ErrorMessage {
-  private int statusCode;
-  private Date timestamp;
-  private String message;
-  private String description;
 
-  public ErrorMessage(int statusCode, Date timestamp, String message, String description) {
-    this.statusCode = statusCode;
-    this.timestamp = timestamp;
-    this.message = message;
-    this.description = description;
-  }
+	private Date timestamp;
+	private int statusCode;
+	private String error;
+	private String exception;
+	private String message;
+	private String uriDescription;
 
-  public int getStatusCode() {
-    return statusCode;
-  }
+	public ErrorMessage() {
+		super();
+	}
 
-  public Date getTimestamp() {
-    return timestamp;
-  }
+	public ErrorMessage(Date timestamp, int statusCode, String error, String exception, String message,
+			String uriDescription) {
+		super();
+		this.timestamp = timestamp;
+		this.statusCode = statusCode;
+		this.error = error;
+		this.exception = exception;
+		this.message = message;
+		this.uriDescription = uriDescription;
+	}
 
-  public String getMessage() {
-    return message;
-  }
+	public Date getTimestamp() {
+		return timestamp;
+	}
 
-  public String getDescription() {
-    return description;
-  }
+	public void setTimestamp(Date timestamp) {
+		this.timestamp = timestamp;
+	}
+
+	public int getStatusCode() {
+		return statusCode;
+	}
+
+	public void setStatusCode(int statusCode) {
+		this.statusCode = statusCode;
+	}
+
+	public String getError() {
+		return error;
+	}
+
+	public void setError(String error) {
+		this.error = error;
+	}
+
+	public String getException() {
+		return exception;
+	}
+
+	public void setException(String exception) {
+		this.exception = exception;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public String getUriDescription() {
+		return uriDescription;
+	}
+
+	public void setUriDescription(String uriDescription) {
+		this.uriDescription = uriDescription;
+	}
+
 }
