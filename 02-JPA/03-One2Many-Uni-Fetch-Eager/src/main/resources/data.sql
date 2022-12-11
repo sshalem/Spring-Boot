87f784fcfdabe9d3ead9eaa605abb4cb9f80d41a
sshalem <shabtay.shalem@gmail.com>
2022-12-11 14:02:42 +0200

    11-12-2022 14:00

--- 02-JPA/03-One2Many-Uni-Fetch-Eager/src/main/resources/data.sql
+++ 02-JPA/03-One2Many-Uni-Fetch-Eager/src/main/resources/data.sql
@@ -29,28 +29,53 @@
 --Insert into USERS_TB --
 -------------------------
 
-INSERT INTO users_tb(id , pid ,name ,email, password) VALUES (1, 1111 , 'shabtay shalem' , 'shabtay.shalem@gmail.com' ,123);
-INSERT INTO users_tb(id , pid ,name ,email, password) VALUES (2, 2222 , 'karin shalem' , 'karin.shalem@gmail.com' ,456);
-INSERT INTO users_tb(id , pid ,name ,email, password) VALUES (3, 3333 , 'avigail shalem' , 'avigail.shalem@gmail.com' ,789);
-INSERT INTO users_tb(id , pid ,name ,email, password) VALUES (4, 4444 , 'odel shalem' , 'odel.shalem@gmail.com' ,012);
+INSERT INTO users_tb(pid ,name ,email, password) VALUES (1111 , 'shabtay shalem' , 'shabtay.shalem@gmail.com' ,123);
+INSERT INTO users_tb(pid ,name ,email, password) VALUES (2222 , 'karin shalem' , 'karin.shalem@gmail.com' ,456);
+INSERT INTO users_tb(pid ,name ,email, password) VALUES (3333 , 'avigail shalem' , 'avigail.shalem@gmail.com' ,789);
+INSERT INTO users_tb(pid ,name ,email, password) VALUES (4444 , 'odel shalem' , 'odel.shalem@gmail.com' ,012);
+
+-- This creates duplication erro in postgres thus I use the above query w/o initializing the id field, 
+-- OtherwiseIn need to sync postgresql DB (See commects in my GIT hub)
+
+-- INSERT INTO users_tb(id , pid ,name ,email, password) VALUES (1, 1111 , 'shabtay shalem' , 'shabtay.shalem@gmail.com' ,123);
+-- INSERT INTO users_tb(id , pid ,name ,email, password) VALUES (2, 2222 , 'karin shalem' , 'karin.shalem@gmail.com' ,456);
+-- INSERT INTO users_tb(id , pid ,name ,email, password) VALUES (3, 3333 , 'avigail shalem' , 'avigail.shalem@gmail.com' ,789);
+-- INSERT INTO users_tb(id , pid ,name ,email, password) VALUES (4, 4444 , 'odel shalem' , 'odel.shalem@gmail.com' ,012);
 
 -------------------------
 -- Insert into ROLES_TB
 -------------------------
-INSERT INTO roles_tb(id , pid ,role ,user_id) VALUES (1, 1111 , 'ADMIN' , 1);
-INSERT INTO roles_tb(id , pid ,role ,user_id) VALUES (2, 1111 , 'SUPER-ADMIN' , 1);
-INSERT INTO roles_tb(id , pid ,role ,user_id) VALUES (3, 1111 , 'MANAGER' , 1);
-INSERT INTO roles_tb(id , pid ,role ,user_id) VALUES (4, 1111 , 'CEO' , 1);
 
-INSERT INTO roles_tb(id , pid ,role ,user_id) VALUES (5, 2222 , 'ADMIN' , 2);
-INSERT INTO roles_tb(id , pid ,role ,user_id) VALUES (6, 2222 , 'SUPER-ADMIN' , 2);
-INSERT INTO roles_tb(id , pid ,role ,user_id) VALUES (7, 2222 , 'MANAGER' , 2);
-INSERT INTO roles_tb(id , pid ,role ,user_id) VALUES (8, 2222 , 'CEO' , 2);
+INSERT INTO roles_tb(pid ,role ,user_id) VALUES (1111 , 'ADMIN' , 1);
+INSERT INTO roles_tb(pid ,role ,user_id) VALUES (1111 , 'SUPER-ADMIN' , 1);
+INSERT INTO roles_tb(pid ,role ,user_id) VALUES (1111 , 'MANAGER' , 1);
+INSERT INTO roles_tb(pid ,role ,user_id) VALUES (1111 , 'CEO' , 1);
 
-INSERT INTO roles_tb(id , pid ,role ,user_id) VALUES (9, 3333 , 'ADMIN' , 3);
-INSERT INTO roles_tb(id , pid ,role ,user_id) VALUES (10, 3333 , 'MANAGER' , 3);
+INSERT INTO roles_tb(pid ,role ,user_id) VALUES (2222 , 'ADMIN' , 2);
+INSERT INTO roles_tb(pid ,role ,user_id) VALUES (2222 , 'SUPER-ADMIN' , 2);
+INSERT INTO roles_tb(pid ,role ,user_id) VALUES (2222 , 'MANAGER' , 2);
+INSERT INTO roles_tb(pid ,role ,user_id) VALUES (2222 , 'CEO' , 2);
 
-INSERT INTO roles_tb(id , pid ,role ,user_id) VALUES (11, 4444 , 'ADMIN' , 4);
-INSERT INTO roles_tb(id , pid ,role ,user_id) VALUES (12, 4444 , 'MANAGER' , 4);
+INSERT INTO roles_tb(pid ,role ,user_id) VALUES (3333 , 'ADMIN' , 3);
+INSERT INTO roles_tb(pid ,role ,user_id) VALUES (3333 , 'MANAGER' , 3);
+
+INSERT INTO roles_tb(pid ,role ,user_id) VALUES (4444 , 'ADMIN' , 4);
+INSERT INTO roles_tb(pid ,role ,user_id) VALUES (4444 , 'MANAGER' , 4);
+
+-- INSERT INTO roles_tb(id , pid ,role ,user_id) VALUES (1, 1111 , 'ADMIN' , 1);
+-- INSERT INTO roles_tb(id , pid ,role ,user_id) VALUES (2, 1111 , 'SUPER-ADMIN' , 1);
+-- INSERT INTO roles_tb(id , pid ,role ,user_id) VALUES (3, 1111 , 'MANAGER' , 1);
+-- INSERT INTO roles_tb(id , pid ,role ,user_id) VALUES (4, 1111 , 'CEO' , 1);
+
+-- INSERT INTO roles_tb(id , pid ,role ,user_id) VALUES (5, 2222 , 'ADMIN' , 2);
+-- INSERT INTO roles_tb(id , pid ,role ,user_id) VALUES (6, 2222 , 'SUPER-ADMIN' , 2);
+-- INSERT INTO roles_tb(id , pid ,role ,user_id) VALUES (7, 2222 , 'MANAGER' , 2);
+-- INSERT INTO roles_tb(id , pid ,role ,user_id) VALUES (8, 2222 , 'CEO' , 2);
+
+-- INSERT INTO roles_tb(id , pid ,role ,user_id) VALUES (9, 3333 , 'ADMIN' , 3);
+-- INSERT INTO roles_tb(id , pid ,role ,user_id) VALUES (10, 3333 , 'MANAGER' , 3);
+
+-- INSERT INTO roles_tb(id , pid ,role ,user_id) VALUES (11, 4444 , 'ADMIN' , 4);
+-- INSERT INTO roles_tb(id , pid ,role ,user_id) VALUES (12, 4444 , 'MANAGER' , 4);
 
 
