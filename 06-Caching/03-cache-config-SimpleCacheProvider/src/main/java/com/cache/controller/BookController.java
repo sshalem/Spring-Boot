859c8819f45e7be43de614febfa930a9bfabc20d
sshalem <shabtay.shalem@gmail.com>
2023-01-26 12:55:53 +0200

    26-01-23 12:55

---
+++ 06-Caching/03-cache-config-SimpleCacheProvider/src/main/java/com/cache/controller/BookController.java
@@ -0,0 +1,67 @@
+package com.cache.controller;
+
+import java.util.List;
+import java.util.Map.Entry;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.Cache;
+import org.springframework.cache.CacheManager;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cache.entity.Book;
+import com.cache.service.BookServiceImpl;
+
+@RestController
+public class BookController {
+
+	@Autowired
+	private BookServiceImpl bookService;
+
+	@Autowired
+	private CacheManager cacheManager;
+
+	@PostMapping("/book")
+	public Book addBook(@RequestBody Book book) {
+		return bookService.addBook(book);
+	}
+
+	@PutMapping("/book")
+	public Book updateBook(@RequestBody Book book) {
+		return bookService.updateBook(book);
+	}
+
+	@GetMapping("/book/{id}")
+	public Book getBook(@PathVariable long id) {
+		return bookService.getBook(id);
+	}
+
+	@GetMapping("/book/getAllBooks")
+	public List<Book> getAllBook() {
+		
+		Cache cache = cacheManager.getCache("booksStore");		
+
+		ConcurrentHashMap<Object, Object> nativeCache = (ConcurrentHashMap<Object, Object>) cache.getNativeCache();
+		Set<Entry<Object, Object>> entrySet = nativeCache.entrySet();
+		
+		entrySet.forEach(e -> {
+			System.out.println(e.getKey());
+			List<Book> value = (List<Book>) e.getValue();
+			value.forEach(i -> System.out.println(i));
+		});
+		
+		return bookService.getAllBooks();
+	}
+
+	@DeleteMapping("/book/{id}")
+	public String deleteBook(@PathVariable long id) {
+		return bookService.deleteBook(id);
+	}
+}
---
+++ 06-Caching/03-cache-config-SimpleCacheProvider/src/main/java/com/cache/entity/Book.java
@@ -0,0 +1,109 @@
+package com.cache.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "book")
+public class Book {
+
+	// I don't want Hibernate to generate the Id
+	// I will do by send it id number with the request body when I send addBook()
+	@Id
+	private long id;
+	private String name;
+	private String category;
+	private String author;
+	private String publisher;
+	private String edition;
+
+	public Book() {
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public String getPublisher() {
+		return publisher;
+	}
+
+	public void setPublisher(String publisher) {
+		this.publisher = publisher;
+	}
+
+	public String getEdition() {
+		return edition;
+	}
+
+	public void setEdition(String edition) {
+		this.edition = edition;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o)
+			return true;
+		if (!(o instanceof Book))
+			return false;
+		Book book = (Book) o;
+		if (getId() != book.getId())
+			return false;
+		if (getName() != null ? !getName().equals(book.getName()) : book.getName() != null)
+			return false;
+		if (getCategory() != null ? !getCategory().equals(book.getCategory()) : book.getCategory() != null)
+			return false;
+		if (getAuthor() != null ? !getAuthor().equals(book.getAuthor()) : book.getAuthor() != null)
+			return false;
+		if (getPublisher() != null ? !getPublisher().equals(book.getPublisher()) : book.getPublisher() != null)
+			return false;
+		return getEdition() != null ? getEdition().equals(book.getEdition()) : book.getEdition() == null;
+	}
+
+	@Override
+	public int hashCode() {
+		int result = (int) (getId() ^ (getId() >>> 32));
+		result = 31 * result + (getName() != null ? getName().hashCode() : 0);
+		result = 31 * result + (getCategory() != null ? getCategory().hashCode() : 0);
+		result = 31 * result + (getAuthor() != null ? getAuthor().hashCode() : 0);
+		result = 31 * result + (getPublisher() != null ? getPublisher().hashCode() : 0);
+		result = 31 * result + (getEdition() != null ? getEdition().hashCode() : 0);
+		return result;
+	}
+
+	@Override
+	public String toString() {
+		return "Book [id=" + id + ", name=" + name + ", category=" + category + ", author=" + author + ", publisher="
+				+ publisher + ", edition=" + edition + "]";
+	}
+	
+}
