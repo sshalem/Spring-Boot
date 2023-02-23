e6d8ab4e76a6889b1518b60f4bea311afadcf3b2
sshalem <shabtay.shalem@gmail.com>
2023-02-23 18:01:30 +0200

    23-02-23 1755

--- 12-SSE-WebSocket/02-websocket-stomp/src/main/java/com/websocekt/stomp/config/WebSocketConfig.java
+++ 12-SSE-WebSocket/02-websocket-stomp/src/main/java/com/websocekt/stomp/config/WebSocketConfig.java
@@ -12,7 +12,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
 	@Override
 	public void configureMessageBroker(MessageBrokerRegistry registry) {
-		registry.enableSimpleBroker("/all", "/specific");
+		registry.enableSimpleBroker("/all");
 		registry.setApplicationDestinationPrefixes("/app");
 	}
 
