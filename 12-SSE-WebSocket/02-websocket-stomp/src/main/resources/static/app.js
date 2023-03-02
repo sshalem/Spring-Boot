86e5ccbfcc793e6586fb80d239e632cfc973a6ff
shabtay shalem <shabtay.shalem@gmail.com>
2023-03-02 12:36:13 +0200

    02-03-23 1235

--- 12-SSE-WebSocket/02-websocket-stomp/src/main/resources/static/app.js
+++ 12-SSE-WebSocket/02-websocket-stomp/src/main/resources/static/app.js
@@ -5,29 +5,15 @@ const conversation = document.getElementById('conversation');
 
 let stompClient = null;
 
-function setConnected(connected) {
-  if (connected) {
-    // $('#conversation').show();
-  } else {
-    // $('#conversation').hide();
-  }
-  // $('#greetings').html('');
-}
-
-function disconnecting() {
-  if (stompClient !== null) {
-    stompClient.disconnect();
-  }
-  setConnected(false);
-  console.log('Disconnected');
-}
-
 connect.addEventListener('click', (e) => {
+  // (1) Try to set up WebSocket connection with the handshake at "http://localhost:8080/ws-stomp-endpoint"
   let socket = new SockJS('http://localhost:8080/ws-stomp-endpoint');
 
   // (2) Create a new StompClient object with the WebSocket endpoint
   stompClient = Stomp.over(socket);
 
+  // (3) Start the STOMP communications, provide a callback for, when the CONNECT frame arrives.
+  //     this is the format of connect: stompClient.connect(header, onConnected, onError);
   stompClient.connect(
     { 'connection-Header': 'connection-Header' },
     function (frame) {
@@ -44,21 +30,54 @@ connect.addEventListener('click', (e) => {
   );
 });
 
-sendMessage.addEventListener('click', (e) => {
-  let messageInput = document.getElementById('message-input').value;
-  const messageToSend = {
-    message: messageInput,
-  };
-  stompClient.send('/app/application', { 'send-Header': 'send-Header' }, JSON.stringify(messageToSend));
-});
-
 disconnect.addEventListener('click', (e) => {
-  disconnect();
+  if (stompClient !== null) {
+    stompClient.disconnect();
+  }
+  setConnected(false);
 });
 
+// (4) Take the value in the 'message-input' text field and send it to the server.
+sendMessage.addEventListener('click', (e) => {
+  let messageInput = document.getElementById('message-input');
+  const messageToSend = {
+    message: messageInput.value,
+  };
+
+  // this is the format of send:
+  // stompClient.send(destination, callback, headers)
+  stompClient.send('/app/application', { 'send-Header': 'send-Header' }, JSON.stringify(messageToSend));
+
+  messageInput.value = '';
+});
+
+// This eventListener is for sending the message when pressing the Enter Key
+document.getElementById('message-input').addEventListener('keyup', (e) => {
+  let messageInput = document.getElementById('message-input');
+  const messageToSend = {
+    message: messageInput.value,
+  };
+
+  if (e.key === 'Enter' && messageInput !== '') {
+    stompClient.send('/app/application', { 'send-Header': 'send-Header' }, JSON.stringify(messageToSend));
+    messageInput.value = '';
+  }
+});
+
+// Helper Methods
 function displayResult(msg) {
   const response = document.getElementById('messages');
   const p = document.createElement('p');
   p.innerHTML = 'message: ' + msg.message;
   response.appendChild(p);
+}
+
+function setConnected(connected) {
+  if (connected) {
+    disconnect.classList.toggle('hide');
+    connect.classList.toggle('hide');
+  } else {
+    disconnect.classList.toggle('hide');
+    connect.classList.toggle('hide');
+  }
 }
