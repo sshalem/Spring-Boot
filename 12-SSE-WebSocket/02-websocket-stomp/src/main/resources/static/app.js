de1a3f262b119ee32c8f9ff494909f8e5ad2921c
sshalem <shabtay.shalem@gmail.com>
2023-03-02 21:57:58 +0200

    02-03-23 2157

--- 12-SSE-WebSocket/02-websocket-stomp/src/main/resources/static/app.js
+++ 12-SSE-WebSocket/02-websocket-stomp/src/main/resources/static/app.js
@@ -1,40 +1,47 @@
-const connect = document.getElementById('connect');
+const connectSection = document.getElementById('connect-section');
+const connectBtn = document.getElementById('connect-btn');
 const disconnect = document.getElementById('disconnect');
 const sendMessage = document.getElementById('sendMessage');
 const conversation = document.getElementById('conversation');
+const usernameInput = document.getElementById('username-input');
 
 // Globaly define this variable
 let stompClient = null;
 
-connect.addEventListener('click', (e) => {
-  // (1) Try to set up WebSocket connection with the handshake at "http://localhost:8080/ws-stomp-endpoint"
-  let socket = new SockJS('http://localhost:8080/ws-stomp-endpoint');
+connectBtn.addEventListener('click', (e) => {
+  if (usernameInput.value === '') {
+    alert('must write a name here before connecting');
+  } else {
+    // (1) Try to set up WebSocket connection with the handshake at "http://localhost:8080/ws-stomp-endpoint"
+    let socket = new SockJS('http://localhost:8080/ws-stomp-endpoint');
 
-  // (2) Create a new StompClient object with the WebSocket endpoint
-  stompClient = Stomp.over(socket);
+    // (2) Create a new StompClient object with the WebSocket endpoint
+    stompClient = Stomp.over(socket);
 
-  // (3) Start the STOMP communications, provide a callback for, when the CONNECT frame arrives.
-  //     this is the format of connect: stompClient.connect(header, onConnected, onError);
-  stompClient.connect(
-    { 'connection-Header': 'connection-Header' },
-    function (frame) {
-      setConnected(true);
-      console.log('Connected: ' + frame);
-      stompClient.subscribe('/all/messages', (result) => {
-        console.log(result);
-        displayResult(JSON.parse(result.body));
-      });
-    },
-    function (error) {
-      console.error(error);
-    }
-  );
+    // (3) Start the STOMP communications, provide a callback for, when the CONNECT frame arrives.
+    //     this is the format of connect: stompClient.connect(header, onConnected, onError);
+    stompClient.connect(
+      { 'connection-Header': 'connection-Header' },
+      function (frame) {
+        setConnected(true);
+        console.log('Connected: ' + frame);
+        stompClient.subscribe('/all/messages', (result) => {
+          console.log(result);
+          displayResult(JSON.parse(result.body));
+        });
+      },
+      function (error) {
+        console.error(error);
+      }
+    );
+  }
 });
 
 disconnect.addEventListener('click', (e) => {
   if (stompClient !== null) {
     stompClient.disconnect();
   }
+  usernameInput.value = '';
   setConnected(false);
 });
 
@@ -69,16 +76,21 @@ document.getElementById('message-input').addEventListener('keyup', (e) => {
 function displayResult(msg) {
   const response = document.getElementById('messages');
   const p = document.createElement('p');
-  p.innerHTML = 'message: ' + msg.message;
+  p.innerHTML = `
+  <label style="margin-right:2rem; font-size:1.7rem">${usernameInput.value} : </label>  ${msg.message}`;
   response.appendChild(p);
 }
 
 function setConnected(connected) {
   if (connected) {
     disconnect.classList.toggle('hide');
-    connect.classList.toggle('hide');
+    connectSection.classList.toggle('hide');
+    // disconnect.classList.remove('hide');
+    // connectSection.classList.add('hide');
   } else {
     disconnect.classList.toggle('hide');
-    connect.classList.toggle('hide');
+    connectSection.classList.toggle('hide');
+    // disconnect.classList.add('hide');
+    // connectSection.classList.remove('hide');
   }
 }
