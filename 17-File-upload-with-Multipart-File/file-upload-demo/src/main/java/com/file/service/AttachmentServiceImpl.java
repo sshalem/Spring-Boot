910b96c34ca12ef2be2ea37595be297560b3fde3
shabtay shalem <shabtay.shalem@gmail.com>
2023-10-17 10:44:02 +0300

    17-10-2023 10:44

--- 17-File-upload-with-Multipart-File/file-upload-demo/src/main/java/com/file/service/AttachmentServiceImpl.java
+++ 17-File-upload-with-Multipart-File/file-upload-demo/src/main/java/com/file/service/AttachmentServiceImpl.java
@@ -16,22 +16,6 @@ public class AttachmentServiceImpl implements AttachmentService {
 
     @Override
     public Attachment saveAttachment(MultipartFile file) throws Exception {
-        System.out.println(file);
-        System.out.println(file.getOriginalFilename());
-        byte[] bytes = file.getBytes();
-        System.out.println(file.getContentType());
-        System.out.println(file.getSize());
-
-        String name = file.getName();
-        System.out.println(name);
-
-        Resource resource = file.getResource();
-        System.out.println(file.getResource());
-
-
-        System.out.println(file.getInputStream());
-
-        System.out.println();
 
         String fileName = StringUtils.cleanPath(file.getOriginalFilename());
 
