1362bc35ce14dd2145a6dd7b0fbb2e3aa49b149e
Shabtay Shalem <shabtay.shalem@asml.com>
2023-10-18 04:47:34 +0300

    18-10-2023  4:47

---
+++ 17-File-upload-with-Multipart-File/file-upload-file-system-simple-setup/src/main/java/com/filesystem/controller/FileUploadController.java
@@ -0,0 +1,27 @@
+package com.filesystem.controller;
+
+import java.io.File;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+public class FileUploadController {
+
+	@PostMapping("/upload")
+	public ResponseEntity<?> handleFileUpload(@RequestParam("file") MultipartFile file) {
+		
+		String fileName = file.getOriginalFilename();
+		
+		try {
+			file.transferTo(new File("C:\\Localdata\\" + fileName));
+		} catch (Exception e) {
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+		}
+		return ResponseEntity.ok("File uploaded successfully.");
+
+	}
+}
