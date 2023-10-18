61bec35be1a27ca9ed93135ee75b34c10eb642f9
Shabtay Shalem <shabtay.shalem@asml.com>
2023-10-18 05:10:49 +0300

    18-10-2023  5:10

---
+++ 17-File-upload-with-Multipart-File/file-upload-File-System/src/main/java/com/file/system/FileUploadFileSystemApplication.java
@@ -0,0 +1,13 @@
+package com.file.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileUploadFileSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileUploadFileSystemApplication.class, args);
+	}
+
+}
