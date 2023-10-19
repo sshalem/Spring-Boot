39be0dfdaa9ffdb039a397d16c0f7ad0acc13489
Shabtay Shalem <shabtay.shalem@asml.com>
2023-10-19 05:29:01 +0300

    19-10-2023  5:29

--- 17-File-upload-with-Multipart-File/file-upload-File-System/src/main/java/com/filesystem/service/FileService.java
+++ 17-File-upload-with-Multipart-File/file-upload-File-System/src/main/java/com/filesystem/service/FileService.java
@@ -2,8 +2,6 @@ package com.filesystem.service;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
