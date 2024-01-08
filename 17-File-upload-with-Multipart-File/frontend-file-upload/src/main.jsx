67ade5798cec52497656fe1a0af66641322577b3
sshalem <shabtay.shalem@gmail.com>
2024-01-08 18:26:51 +0200

    08-01-2024 18:26

--- 17-File-upload-with-Multipart-File/frontend-file-upload/src/main.jsx
+++ 17-File-upload-with-Multipart-File/frontend-file-upload/src/main.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
 import './index.css';
+
+import App from './projects/01-using-form-to-upload/App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <>
