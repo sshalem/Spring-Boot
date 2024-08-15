47269a0923d2313591fdde2ceb988e2f8bff55a0
shabtay shalem <shabtay.shalem@gmail.com>
2024-08-16 01:08:20 +0300

    16-08-2024  1:08

--- Git_all.bat
+++ Git_all.bat
@@ -1,112 +1,28 @@
 @echo off
-echo.
-echo.
-echo.
-echo.
-echo.
-echo.
-echo.           *****************************
-echo.           * step 1 : git pull --all   *
-echo.           ***************************** 
-@echo on
+echo.[33mgit pull --all    [0m
 git pull --all
-@echo off
-::pause
-::cls
-@echo off
 echo.
-echo.
-echo.
-echo.
-echo.
-echo.
-echo.
-echo.           *************************
-echo.           * step 2 : git status   *
-echo.           ************************* 
-@echo on
+echo.[33mgit status        [0m
 git status
-@echo off
-::cls
-
-@echo off
 echo.
-echo.
-echo.
-echo.
-echo.
-echo.
-echo.
-echo.           ****************************
-echo.           * step 3 : git add --all   *
-echo.           **************************** 
-@echo on
+echo.[33mgit add --all     [0m
 git add --all
-@echo off
-::cls
-@echo off
 echo.
-echo.
-echo.
-echo.
-echo.
-echo.
-echo.
-echo.           *************************
-echo.           * step 4 : git status   *
-echo.           ************************* 
-@echo on
+echo.[33mgit status        [0m
 git status
-@echo off
-::pause
-::cls
-@echo off
-echo.
-echo.
-echo.
-echo.
-echo.
-echo.
-echo.
-::pause
-::echo.
-::echo.
-::echo.
-::echo.
-::echo.
-::echo.
-::echo.
-::echo *********************************************
-::echo *	%DATE% , %TIME%	    *
-::echo *********************************************
-::echo * step 5 : git commit -m "%DATE:~0,2%-%DATE:~3,2%-%DATE:~6,4% %TIME:~0,2%:%TIME:~3,2%" *
-::echo *********************************************
-::echo.
-::echo.
-::git commit -m "%DATE:~0,2%-%DATE:~3,2%-%DATE:~6,4% %TIME:~0,2%:%TIME:~3,2%"
-echo.
-echo.
-echo.
 echo.
 if %ComputerName%==LAPTOP-AAAA (        
-    echo.           *********************************************
-    echo.           * computer name is = %ComputerName%            *    
-    echo.           *********************************************
-    echo.           *	%DATE% , %TIME%	    *
-    echo.           *********************************************
-    echo.           * step 5 : git commit -m "%DATE:~0,2%-%DATE:~3,2%-%DATE:~6,4% %TIME:~0,2%:%TIME:~3,2%" *
-    echo.           *********************************************
-    echo.
-    echo.
-    @echo on
+    echo.[33mgit commit -m "%DATE:~0,2%-%DATE:~3,2%-%DATE:~6,4% %TIME:~0,2%:%TIME:~3,2%"  [0m
+    echo.   [36mcomputer name is = %ComputerName% [0m
+    echo.   [36m%DATE% , %TIME%	 [0m
     git commit -m "%DATE:~0,2%-%DATE:~3,2%-%DATE:~6,4% %TIME:~0,2%:%TIME:~3,2%"     
 ) else if %ComputerName%==DESKTOP-T4E706U (
     echo.           *********************************************
+    echo.           * step 5 : git commit -m "%DATE:~7,2%-%DATE:~4,2%-%DATE:~10,4% %TIME:~0,2%:%TIME:~3,2%" *
+    echo.           *********************************************
     echo.           * computer name is = %ComputerName%            *    
     echo.           *********************************************
     echo.           *	%DATE% , %TIME%	    *
-    echo.           *********************************************
-    echo.           * step 5 : git commit -m "%DATE:~7,2%-%DATE:~4,2%-%DATE:~10,4% %TIME:~0,2%:%TIME:~3,2%" *
     echo.           *********************************************
     echo.
     echo.
@@ -114,11 +30,11 @@ if %ComputerName%==LAPTOP-AAAA (
     git commit -m "%DATE:~7,2%-%DATE:~4,2%-%DATE:~10,4% %TIME:~0,2%:%TIME:~3,2%"   
 ) else if %ComputerName%==ASUS (
     echo.           *********************************************
+    echo.           * step 5 : git commit -m "%DATE:~7,2%-%DATE:~4,2%-%DATE:~12,4% %TIME:~0,2%:%TIME:~3,2%" *
+    echo.           *********************************************
     echo.           * computer name is = %ComputerName%            *    
     echo.           *********************************************
     echo.           *	%DATE% , %TIME%	    *
-    echo.           *********************************************
-    echo.           * step 5 : git commit -m "%DATE:~7,2%-%DATE:~4,2%-%DATE:~12,4% %TIME:~0,2%:%TIME:~3,2%" *
     echo.           *********************************************
     echo.
     echo.
@@ -126,11 +42,11 @@ if %ComputerName%==LAPTOP-AAAA (
     git commit -m "%DATE:~7,2%-%DATE:~4,2%-%DATE:~12,4% %TIME:~0,2%:%TIME:~3,2%"      
 ) else if %ComputerName%==LT461676 (
     echo.           *********************************************
+    echo.           * step 5 : git commit -m "%DATE:~0,2%-%DATE:~3,2%-%DATE:~6,4% %TIME:~0,2%:%TIME:~3,2%" *
+    echo.           *********************************************
     echo.           * computer name is = %ComputerName%            *    
     echo.           *********************************************
     echo.           *	%DATE% , %TIME%	    *
-    echo.           *********************************************
-    echo.           * step 5 : git commit -m "%DATE:~0,2%-%DATE:~3,2%-%DATE:~6,4% %TIME:~0,2%:%TIME:~3,2%" *
     echo.           *********************************************
     echo.
     echo.
@@ -138,53 +54,28 @@ if %ComputerName%==LAPTOP-AAAA (
     git commit -m "%DATE:~0,2%-%DATE:~3,2%-%DATE:~6,4% %TIME:~0,2%:%TIME:~3,2%"  
     @REM @echo off
 ) else (
+    echo.          [31m      Warning !!!
     echo.                Warning !!!
     echo.                Warning !!!
-    echo.                Warning !!!
     echo.
-    echo.                ************************************************
-    echo.                * Batch CODE didn't recognize the computer name *
-    echo.                * Thus git commit wasn't executed               *
-    echo.                * add it to the BATCH code                      *
-    echo.                * PLease add computer name  %ComputerName% to CODE  *    
-    echo.                *************************************************
-    echo.
-    echo.
-    echo            ...  git push --all  --> Not executed executed
+    echo.                *************************************************************
+    echo.                * Batch CODE didn't recognize the computer name 
+    echo.                * Thus "git commit" wasn't executed               
+    echo.                * add it to the BATCH code                      
+    echo.                * PLease add computer name  %ComputerName% to CODE      
+    echo.                *************************************************************
     echo.
     echo.
+    echo.                "git push" Not executed executed
+    echo.
+    echo.         [0m
     echo.
     echo.
     cmd /k
 )
 
-
-@echo off
-::pause
-::@echo off
 echo.
-echo.
-echo.
-echo.
-echo.
-echo.
-echo.
-echo.               *******************************
-echo.               * step 6 :  git push --all    *						
-echo.               *******************************
-@echo on
+echo.[33mgit push --all   [0m
 git push --all
-@echo off
-echo.
-echo.
-echo.
-echo.
-echo.
-echo.
-echo.            .......completed git update successfuly.......
-echo.
-echo.
-echo.
-echo.
+echo.[33m .......completed git update successfuly.......[0m
 cmd /k
-
