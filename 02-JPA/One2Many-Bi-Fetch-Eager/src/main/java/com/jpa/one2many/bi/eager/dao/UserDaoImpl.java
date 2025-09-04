c167f90a73deccc79627fe1c459fae8bd20e54e1
Shabtay Shalem <shabtay.shalem@asml.com>
2025-09-04 07:22:31 +0300

    09-04-2025  7:22

--- 02-JPA/One2Many-Bi-Fetch-Eager/src/main/java/com/jpa/one2many/bi/eager/dao/UserDaoImpl.java
+++ 02-JPA/One2Many-Bi-Fetch-Eager/src/main/java/com/jpa/one2many/bi/eager/dao/UserDaoImpl.java
@@ -9,6 +9,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.jpa.one2many.bi.eager.entity.RoleEntity;
 import com.jpa.one2many.bi.eager.entity.UserEntity;
 import com.jpa.one2many.bi.eager.exception.ResourceNotFoundException;
+import com.jpa.one2many.bi.eager.repository.RoleRepository;
 import com.jpa.one2many.bi.eager.repository.UserRepository;
 
 @Service
@@ -17,6 +18,9 @@ public class UserDaoImpl implements UserDao {
 	@Autowired
 	private UserRepository userRepository;
 
+	@Autowired
+	private RoleRepository roleRepository;
+
 	@Override
 	public UserEntity createUser(UserEntity userEntity) {
 		return userRepository.save(userEntity);
@@ -24,10 +28,10 @@ public class UserDaoImpl implements UserDao {
 
 	@Override
 	public UserEntity getUserById(long id) {
-		// UserEntity userEntity = userRepository.findById(id);
-		// UserEntity userEntity = userRepository.nativeFindById(id);
+		//	UserEntity userEntity = userRepository.findById(id);
+		//	UserEntity userEntity =  userRepository.nativeFindById(id);
 		UserEntity userEntity = userRepository.jpqlFindById(id);
-		if (userEntity == null)
+		if(userEntity == null)
 			throw new ResourceNotFoundException("Not found User with id = " + id);
 		return userEntity;
 	}
@@ -35,7 +39,7 @@ public class UserDaoImpl implements UserDao {
 	@Override
 	public UserEntity getUserByPid(long pid) {
 		UserEntity userEntity = userRepository.findByPid(pid);
-		if (userEntity == null)
+		if(userEntity == null)
 			throw new ResourceNotFoundException("Not found User with pid = " + pid);
 		return userEntity;
 	}
@@ -43,7 +47,7 @@ public class UserDaoImpl implements UserDao {
 	@Override
 	public UserEntity getUserByName(String name) {
 		UserEntity userEntity = userRepository.findByName(name);
-		if (userEntity == null)
+		if(userEntity == null)
 			throw new ResourceNotFoundException("Not found User with name = " + name);
 		return userEntity;
 	}
@@ -51,7 +55,7 @@ public class UserDaoImpl implements UserDao {
 	@Override
 	public UserEntity getUserByEmail(String email) {
 		UserEntity userEntity = userRepository.findByEmail(email);
-		if (userEntity == null)
+		if(userEntity == null)
 			throw new ResourceNotFoundException("Not found User with email = " + email);
 		return userEntity;
 	}
@@ -64,7 +68,7 @@ public class UserDaoImpl implements UserDao {
 	@Override
 	public void removeUserByPid(long pid) {
 		UserEntity userEntity = userRepository.findByPid(pid);
-		if (userEntity == null)
+		if(userEntity == null)
 			throw new ResourceNotFoundException("Not found User with pid = " + pid);
 		userRepository.delete(userEntity);
 	}
@@ -74,24 +78,25 @@ public class UserDaoImpl implements UserDao {
 
 		UserEntity userEntity = userRepository.findByPid(userPid);
 
-		if (userEntity == null)
+		if(userEntity == null)
 			throw new ResourceNotFoundException("Not found User with userPid = " + userPid);
 
 		roleEntity.setPid(userPid);
 
 		userEntity.addRole(roleEntity);
 
-		return userRepository.save(userEntity);
+        return userRepository.save(userEntity);
 	}
 
 	/**
-	 * @Transactional Annotation - Should be only on 'PUBLIC' methods that returns
-	 *                value to higher level layer And : ✅ Rule of Thumb Any method
-	 *                that modifies entities (insert/update/delete) in JPA/Hibernate
-	 *                should usually be transactional so Hibernate can flush changes
-	 *                . Read-only operations can be non-transactional (Unless I use
-	 *                Fetch.Lazy), but updates need @Transactional so Hibernate can
-	 *                flush changes.
+	 * @Transactional Annotation -
+	 * 			Should be only on
+	 * 			'PUBLIC' methods that returns value to higher level layer
+	 * 		And :
+	 * 		✅ Rule of Thumb
+	 * 			Any method that modifies entities (insert/update/delete) in JPA/Hibernate should usually be transactional
+	 * 			so Hibernate can flush changes	.
+	 * 			Read-only operations can be non-transactional (Unless I use Fetch.Lazy), but updates need @Transactional so Hibernate can flush changes.
 	 */
 
 	@Override
@@ -99,40 +104,46 @@ public class UserDaoImpl implements UserDao {
 	public UserEntity removeRoleFromUser(long userPid, String role) {
 
 		/**
-		 * In this Implementation 1. I add the orphanRemoval to UserEntity One2Many 2. I
-		 * Query For RoleEntity (I try with 2 different implementations) 3. remove the
-		 * Entity from the SET<RoleEntity> collection 4. Save the the info to UserEntity
+		 * In this Implementation
+		 * 1. I add the orphanRemoval to UserEntity One2Many
+		 * 2. I Query For RoleEntity (I try with 2 different implementations)
+		 * 3. remove the Entity from the SET<RoleEntity> collection
+		 * 4. Save the the info to UserEntity
 		 * 5. I must add @Transactional annotation to the method `removeRoleFromUser()`
 		 * 
-		 * ✅ Rule of Thumb Any method that modifies entities (insert/update/delete) in
-		 * JPA/Hibernate should usually be transactional so Hibernate can flush changes
-		 * . Read-only operations can be non-transactional (Unless I use Fetch.Lazy),
-		 * but updates need @Transactional so Hibernate can flush changes.
+		 * 		✅ Rule of Thumb
+		 * 			Any method that modifies entities (insert/update/delete) in JPA/Hibernate should usually be transactional
+		 * 			so Hibernate can flush changes	.
+		 * 			Read-only operations can be non-transactional (Unless I use Fetch.Lazy), but updates need @Transactional so Hibernate can flush changes.
 		 */
 
 		UserEntity userEntity = userRepository.findByPid(userPid);
 
-		if (userEntity == null)
+		if(userEntity == null)
 			throw new ResourceNotFoundException("Not found User with userPid = " + userPid);
 
 		/**
 		 * there are 4 different ways to retrieve roleEntity from DB
 		 */
 
-		/**
-		 * @Transactional Annotation - Should be only on 'PUBLIC' methods that returns
-		 *                value to higher level layer And : ✅ Rule of Thumb Any method
-		 *                that modifies entities (insert/update/delete) in JPA/Hibernate
-		 *                should usually be transactional so Hibernate can flush changes
-		 *                . Read-only operations can be non-transactional (Unless I use
-		 *                Fetch.Lazy), but updates need @Transactional so Hibernate can
-		 *                flush changes.
-		 */
 
 		/**
-		 * (1) - Not best approach We Don't need to add @Transactional If we search with
-		 * For loop With this Implementation , NO NEED orphanRemoval = true on
-		 * the @OneToMany also @OneToMany is W/O CascadeType.REMOVE
+		 * @Transactional Annotation -
+		 * 			Should be only on
+		 * 			'PUBLIC' methods that returns value to higher level layer
+		 * 		And :
+		 * 		✅ Rule of Thumb
+		 * 			Any method that modifies entities (insert/update/delete) in JPA/Hibernate should usually be transactional
+		 * 			so Hibernate can flush changes	.
+		 * 			Read-only operations can be non-transactional (Unless I use Fetch.Lazy), but updates need @Transactional so Hibernate can flush changes.
+		 */
+
+
+		/**
+		 * (1) - Not best approach
+		 * We Don't need to add @Transactional If we search with For loop
+		 * With this Implementation , NO NEED  orphanRemoval = true on the @OneToMany
+		 * also @OneToMany is W/O CascadeType.REMOVE
 		 */
 //		Set<RoleEntity> roles = userEntity.getRoles();
 //
@@ -148,6 +159,7 @@ public class UserDaoImpl implements UserDao {
 //			}
 //		}
 
+
 //		// In this approach
 //		// [1] I remove the role from the collection in memory
 //		// [2] I explicitly delete roleEntity from Role_DB
@@ -156,48 +168,49 @@ public class UserDaoImpl implements UserDao {
 //		roleRepository.delete(roleEntity);
 //		return userEntity;
 
+
 		/**
-		 * (2) Query from UserRepo will work only if orphanRemoval = true. Note:
-		 * CascadeType.REMOVE ,only propagates when the parent entity itself is removed.
-		 * and Here I remove a child (role), not parent (user) , Thus, Doesn't matter in
-		 * this situation if Parent (User) is with or w/o CascadeType.REMOVE
+		 * (2)
+		 * Query from UserRepo will work only if orphanRemoval = true.
+		 * Note: CascadeType.REMOVE ,only propagates when the parent entity itself is removed.
+		 * and Here I remove a child (role), not parent (user) ,
+		 * Thus, Doesn't matter in this situation if Parent (User) is with or w/o CascadeType.REMOVE
 		 */
 		RoleEntity roleEntity = userRepository.getRoleByIdAndRole(userEntity.getId(), role);
 		userEntity.removeRole(roleEntity);
 
-		// WHen I config orphanRemoval = flase , and NO CASCADE , I need do
+		
+		// WHen I config orphanRemoval = flase , and NO CASCADE , I need do 
 		// do this line : roleRepository.save(roleEntity);
 		// and not this line userRepository.save(userEntity);
-
-		// Question why:
-		// Why I do roleRepository.save(roleEntity) and No doing
-		// userRepository.save(userEntity)
-		// Answer : this is so I won't delete the role from DB, but I set it to null ,
-		// when I do the line of userEntity.removeRole(roleEntity);
-
-		// But it still deleting role From DB : rrrrrrrrrrrrrrrrrrrrrrr
+		
+		// Question why: 		
+		// Why I do roleRepository.save(roleEntity) and No doing  userRepository.save(userEntity)
+		// Answer : this is so I won't delete the role from DB, but I set it to null , when I do the line of userEntity.removeRole(roleEntity);
+		
+		// But it still deleting role From DB : rrrrrrrrrrrrrrrrrrrrrrr  
 
 		userRepository.save(userEntity);
 //		 roleRepository.save(roleEntity);
 		return userEntity;
 
 		/**
-		 * (3) Query from RoleRepo * Query from UserRepo will work only if orphanRemoval
-		 * = true. * Note: CascadeType.REMOVE ,only propagates when the parent entity
-		 * itself is removed. * and Here I remove a child (role), not parent (user) , *
-		 * Thus, Doesn't matter in this situation if Parent (User) is with or w/o
-		 * CascadeType.REMOVE
+		 * (3) Query from RoleRepo
+         * 		 * Query from UserRepo will work only if orphanRemoval = true.
+         * 		 * Note: CascadeType.REMOVE ,only propagates when the parent entity itself is removed.
+         * 		 * and Here I remove a child (role), not parent (user) ,
+         * 		 * Thus, Doesn't matter in this situation if Parent (User) is with or w/o CascadeType.REMOVE
 		 */
 //		RoleEntity roleEntity = roleRepository.jpqlFindRoleByPidAndRoleName(userPid, role);
 //		userEntity.removeRole(roleEntity);
 //        return userRepository.save(userEntity);
 
 		/**
-		 * (4) Query from RoleRepo * Query from UserRepo will work only if orphanRemoval
-		 * = true. * Note: CascadeType.REMOVE ,only propagates when the parent entity
-		 * itself is removed. * and Here I remove a child (role), not parent (user) , *
-		 * Thus, Doesn't matter in this situation if Parent (User) is with or w/o
-		 * CascadeType.REMOVE
+		 * (4) Query from RoleRepo
+         * 		 * Query from UserRepo will work only if orphanRemoval = true.
+         * 		 * Note: CascadeType.REMOVE ,only propagates when the parent entity itself is removed.
+         * 		 * and Here I remove a child (role), not parent (user) ,
+         * 		 * Thus, Doesn't matter in this situation if Parent (User) is with or w/o CascadeType.REMOVE
 		 */
 //		RoleEntity roleEntity = roleRepository.findByPidAndRole(userPid, role);
 // 		userEntity.removeRole(roleEntity);
---
+++ 02-JPA/One2Many-Bi-Fetch-Eager/src/main/resources/data-H2.sql
@@ -0,0 +1,81 @@
+-----------------------------------
+-- Set FOREIGN KEY CHECK to 0 
+-- TRUNCATE the data from tables
+----------------------------------
+-- SET FOREIGN_KEY_CHECKS=0; 
+-- TRUNCATE TABLE roles_tb;
+-- TRUNCATE TABLE users_tb;
+-- TRUNCATE TABLE tutorial_tags;
+-- TRUNCATE TABLE tutorials;
+-- TRUNCATE TABLE tags;
+-- TRUNCATE TABLE course_student;
+-- TRUNCATE TABLE student_tb;
+-- TRUNCATE TABLE course_tb;
+-- SET FOREIGN_KEY_CHECKS=1;
+
+-----------------------------------
+-- DROP TABLEs if exist
+----------------------------------
+-- DROP TABLE IF EXISTS roles_tb;
+-- DROP TABLE IF EXISTS users_tb;
+-- DROP TABLE IF EXISTS tutorial_tags;
+-- DROP TABLE IF EXISTS tutorials;
+-- DROP TABLE IF EXISTS tags;
+-- DROP TABLE IF EXISTS course_student;
+-- DROP TABLE IF EXISTS student_tb;
+-- DROP TABLE IF EXISTS course_tb;
+
+-------------------------
+--Insert into USERS_TB --
+-------------------------
+
+INSERT INTO users_tb(pid ,name ,email, password) VALUES (1111 , 'shabtay shalem' , 'shabtay.shalem@gmail.com' ,123);
+INSERT INTO users_tb(pid ,name ,email, password) VALUES (2222 , 'karin shalem' , 'karin.shalem@gmail.com' ,456);
+INSERT INTO users_tb(pid ,name ,email, password) VALUES (3333 , 'avigail shalem' , 'avigail.shalem@gmail.com' ,789);
+INSERT INTO users_tb(pid ,name ,email, password) VALUES (4444 , 'odel shalem' , 'odel.shalem@gmail.com' ,012);
+
+-- This creates duplication erro in postgres thus I use the above query w/o initializing the id field, 
+-- OtherwiseIn need to sync postgresql DB (See commects in my GIT hub)
+
+-- INSERT INTO users_tb(id , pid ,name ,email, password) VALUES (1, 1111 , 'shabtay shalem' , 'shabtay.shalem@gmail.com' ,123);
+-- INSERT INTO users_tb(id , pid ,name ,email, password) VALUES (2, 2222 , 'karin shalem' , 'karin.shalem@gmail.com' ,456);
+-- INSERT INTO users_tb(id , pid ,name ,email, password) VALUES (3, 3333 , 'avigail shalem' , 'avigail.shalem@gmail.com' ,789);
+-- INSERT INTO users_tb(id , pid ,name ,email, password) VALUES (4, 4444 , 'odel shalem' , 'odel.shalem@gmail.com' ,012);
+
+-------------------------
+-- Insert into ROLES_TB
+-------------------------
+
+INSERT INTO roles_tb(pid ,role ,user_id) VALUES (1111 , 'ADMIN' , 1);
+INSERT INTO roles_tb(pid ,role ,user_id) VALUES (1111 , 'SUPER-ADMIN' , 1);
+INSERT INTO roles_tb(pid ,role ,user_id) VALUES (1111 , 'MANAGER' , 1);
+INSERT INTO roles_tb(pid ,role ,user_id) VALUES (1111 , 'CEO' , 1);
+
+INSERT INTO roles_tb(pid ,role ,user_id) VALUES (2222 , 'ADMIN' , 2);
+INSERT INTO roles_tb(pid ,role ,user_id) VALUES (2222 , 'SUPER-ADMIN' , 2);
+INSERT INTO roles_tb(pid ,role ,user_id) VALUES (2222 , 'MANAGER' , 2);
+INSERT INTO roles_tb(pid ,role ,user_id) VALUES (2222 , 'CEO' , 2);
+
+INSERT INTO roles_tb(pid ,role ,user_id) VALUES (3333 , 'ADMIN' , 3);
+INSERT INTO roles_tb(pid ,role ,user_id) VALUES (3333 , 'MANAGER' , 3);
+
+INSERT INTO roles_tb(pid ,role ,user_id) VALUES (4444 , 'ADMIN' , 4);
+INSERT INTO roles_tb(pid ,role ,user_id) VALUES (4444 , 'MANAGER' , 4);
+
+-- INSERT INTO roles_tb(id , pid ,role ,user_id) VALUES (1, 1111 , 'ADMIN' , 1);
+-- INSERT INTO roles_tb(id , pid ,role ,user_id) VALUES (2, 1111 , 'SUPER-ADMIN' , 1);
+-- INSERT INTO roles_tb(id , pid ,role ,user_id) VALUES (3, 1111 , 'MANAGER' , 1);
+-- INSERT INTO roles_tb(id , pid ,role ,user_id) VALUES (4, 1111 , 'CEO' , 1);
+
+-- INSERT INTO roles_tb(id , pid ,role ,user_id) VALUES (5, 2222 , 'ADMIN' , 2);
+-- INSERT INTO roles_tb(id , pid ,role ,user_id) VALUES (6, 2222 , 'SUPER-ADMIN' , 2);
+-- INSERT INTO roles_tb(id , pid ,role ,user_id) VALUES (7, 2222 , 'MANAGER' , 2);
+-- INSERT INTO roles_tb(id , pid ,role ,user_id) VALUES (8, 2222 , 'CEO' , 2);
+
+-- INSERT INTO roles_tb(id , pid ,role ,user_id) VALUES (9, 3333 , 'ADMIN' , 3);
+-- INSERT INTO roles_tb(id , pid ,role ,user_id) VALUES (10, 3333 , 'MANAGER' , 3);
+
+-- INSERT INTO roles_tb(id , pid ,role ,user_id) VALUES (11, 4444 , 'ADMIN' , 4);
+-- INSERT INTO roles_tb(id , pid ,role ,user_id) VALUES (12, 4444 , 'MANAGER' , 4);
+
+
