b90d62f302a635daaf250a46ad972817828d1ca9
shabtay shalem <shabtay.shalem@gmail.com>
2025-10-26 01:18:12 +0300

    26-10-2025  1:18

--- 05-Transaction-Management/O1-transaction-management/src/main/java/com/trans/mngnt/exception/ControllerExceptionHandler.java
+++ 05-Transaction-Management/O1-transaction-management/src/main/java/com/trans/mngnt/exception/ControllerExceptionHandler.java
@@ -1,8 +1,14 @@
 package com.trans.mngnt.exception;
 
-import java.util.Date;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
+import java.util.Map;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
@@ -10,33 +16,32 @@ import org.springframework.web.context.request.WebRequest;
 @RestControllerAdvice
 public class ControllerExceptionHandler {
 
-	@ExceptionHandler(ResourceNotFoundException.class)
-	public ErrorMessage resourceNotFoundException(ResourceNotFoundException ex, WebRequest request) {
-
-		ErrorMessage message = new ErrorMessage();
-
-		message.setTimestamp(new Date());
-		message.setStatusCode(HttpStatus.NOT_FOUND.value());
-		message.setError(HttpStatus.valueOf(HttpStatus.NOT_FOUND.value()).getReasonPhrase());
-		message.setException(ex.getClass().getCanonicalName());
-		message.setMessage(ex.getMessage());
-		message.setUriDescription(request.getDescription(false));
-
-		return message;
+	private String getCurrentTimestamp() {
+		ZonedDateTime now = ZonedDateTime.now(ZoneId.of("Asia/Jerusalem")); // or systemDefault()
+		return now.format(DateTimeFormatter.ofPattern("yyyy-MM-dd  HH:mm:ss z"));
 	}
 
-	@ExceptionHandler(Exception.class)
-	public ErrorMessage globalExceptionHandler(Exception ex, WebRequest request) {
+	@ExceptionHandler(value = { ResourceNotFoundException.class })
+	public ResponseEntity<Object> handleUserServiceException(ResourceNotFoundException ex, WebRequest request) {
+		Map<String, Object> response = new HashMap<>();
+		response.put("timestamp", getCurrentTimestamp());
+		response.put("statusCode", HttpStatus.NOT_FOUND.value());
+		response.put("error", HttpStatus.valueOf(HttpStatus.NOT_FOUND.value()).getReasonPhrase());
+		response.put("exception", ResourceNotFoundException.class.getName());
+		response.put("message", ex.getMessage());
+		response.put("uriDescription", request.getDescription(false));
+		return new ResponseEntity<>(response, new HttpHeaders(), HttpStatus.NOT_FOUND);
+	}
 
-		ErrorMessage message = new ErrorMessage();
-
-		message.setTimestamp(new Date());
-		message.setStatusCode(HttpStatus.INTERNAL_SERVER_ERROR.value());
-		message.setError(HttpStatus.valueOf(HttpStatus.INTERNAL_SERVER_ERROR.value()).getReasonPhrase());
-		message.setException(ex.getClass().getCanonicalName());
-		message.setMessage(ex.getMessage());
-		message.setUriDescription(request.getDescription(false));
-
-		return message;
+	@ExceptionHandler(value = { Exception.class })
+	public ResponseEntity<Object> handleOtherExceptions(Exception ex, WebRequest request) {
+		Map<String, Object> response = new HashMap<>();
+		response.put("timestamp", getCurrentTimestamp());
+		response.put("statusCode", HttpStatus.INTERNAL_SERVER_ERROR.value());
+		response.put("error", HttpStatus.valueOf(HttpStatus.INTERNAL_SERVER_ERROR.value()).getReasonPhrase());
+		response.put("exception", ex.getClass().getCanonicalName());
+		response.put("message", ex.getMessage());
+		response.put("uriDescription", request.getDescription(false));
+		return new ResponseEntity<>(response, new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
 	}
 }
