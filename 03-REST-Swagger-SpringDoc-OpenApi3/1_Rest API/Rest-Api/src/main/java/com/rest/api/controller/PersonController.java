0bbe5fabf9b1a78d67b349e776af89596ead954d
shabtay shalem <shabtay.shalem@gmail.com>
2025-11-16 11:47:31 +0200

    16-11-2025 11:47

--- 03-REST-Swagger-SpringDoc-OpenApi3/1_Rest API/Rest-Api/src/main/java/com/rest/api/controller/PersonController.java
+++ 03-REST-Swagger-SpringDoc-OpenApi3/1_Rest API/Rest-Api/src/main/java/com/rest/api/controller/PersonController.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -59,6 +61,8 @@ public class PersonController {
 		params.forEach((i,j) -> {			
 			System.out.println(i + " : " + j);
 		});
-		return null;
+		return new ResponseEntity<>(HttpStatus.NO_CONTENT);
 	}
+	
+	
 }
