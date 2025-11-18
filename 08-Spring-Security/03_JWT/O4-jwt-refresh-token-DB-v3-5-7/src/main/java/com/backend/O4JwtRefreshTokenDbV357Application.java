b4ce98e77ecc3db9c79e209f1e06f1344792aab1
shabtay shalem <shabtay.shalem@gmail.com>
2025-11-18 23:57:24 +0200

    18-11-2025 23:57

---
+++ 08-Spring-Security/03_JWT/O4-jwt-refresh-token-DB-v3-5-7/src/main/java/com/backend/O4JwtRefreshTokenDbV357Application.java
@@ -0,0 +1,13 @@
+package com.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class O4JwtRefreshTokenDbV357Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(O4JwtRefreshTokenDbV357Application.class, args);
+	}
+
+}
---
+++ 08-Spring-Security/03_JWT/O4-jwt-refresh-token-DB-v3-5-7/src/test/java/com/backend/O4JwtRefreshTokenDbV357ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class O4JwtRefreshTokenDbV357ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
