95ea70d01ad964b032343a639e1f0ab1418ea595
Shabtay Shalem <shabtay.shalem@asml.com>
2025-11-19 19:07:08 +0200

    11-19-2025 19:07

--- 08-Spring-Security/03_JWT/O4-jwt-refresh-token-DB/src/main/java/com/backend/O4JwtRefreshTokenDBApplication.java
+++ 08-Spring-Security/03_JWT/O4-jwt-refresh-token-DB/src/main/java/com/backend/O4JwtRefreshTokenDBApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class O4JwtRefreshTokenDbV357Application {
+public class O4JwtRefreshTokenDBApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(O4JwtRefreshTokenDbV357Application.class, args);
+		SpringApplication.run(O4JwtRefreshTokenDBApplication.class, args);
 	}
 
 }
--- 08-Spring-Security/03_JWT/O4-jwt-refresh-token-DB/src/test/java/com/backend/O4JwtRefreshTokenDBApplicationTests.java
+++ 08-Spring-Security/03_JWT/O4-jwt-refresh-token-DB/src/test/java/com/backend/O4JwtRefreshTokenDBApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class O4JwtRefreshTokenDbV357ApplicationTests {
+class O4JwtRefreshTokenDBApplicationTests {
 
 	@Test
 	void contextLoads() {
