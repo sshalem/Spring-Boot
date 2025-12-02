7cfafa2766a271a3adf6b767542d4e034da5a1f5
shabtay shalem <shabtay.shalem@gmail.com>
2025-12-02 12:51:52 +0200

    02-12-2025 12:51

--- 08-Spring-Security/03_JWT/O4-jwt-refresh-token-DB/src/main/java/com/backend/exceptions/ControllerExceptionHandler.java
+++ 08-Spring-Security/03_JWT/O4-jwt-refresh-token-DB/src/main/java/com/backend/exceptions/ControllerExceptionHandler.java
@@ -1,45 +1,50 @@
 package com.backend.exceptions;
 
-import java.util.Date;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
+import java.util.Map;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 
 @RestControllerAdvice
 public class ControllerExceptionHandler {
 
+	private String getCurrentTimestamp() {
+		ZonedDateTime now = ZonedDateTime.now(ZoneId.of("Asia/Jerusalem")); // or systemDefault()
+		return now.format(DateTimeFormatter.ofPattern("yyyy-MM-dd  HH:mm:ss z"));
+	}
+
 	@ExceptionHandler(EmailAlreadyExistException.class)
-	@ResponseStatus(value = HttpStatus.BAD_REQUEST)
-	public ErrorMessage emailAlreadyExistException(EmailAlreadyExistException ex, WebRequest request) {
+	public ResponseEntity<Object> emailAlreadyExistException(EmailAlreadyExistException ex, WebRequest request) {
 
-		ErrorMessage message = new ErrorMessage();
+		Map<String, Object> response = new HashMap<>();
+		response.put("timestamp", getCurrentTimestamp());
+		response.put("statusCode", HttpStatus.NOT_FOUND.value());
+		response.put("error", HttpStatus.valueOf(HttpStatus.NOT_FOUND.value()).getReasonPhrase());
+		response.put("exception", ResourceNotFoundException.class.getName());
+		response.put("message", ex.getMessage());
+		response.put("uriDescription", request.getDescription(false));
+		return new ResponseEntity<>(response, new HttpHeaders(), HttpStatus.NOT_FOUND);
 
-		message.setTimestamp(new Date());
-		message.setStatusCode(HttpStatus.BAD_REQUEST.value());
-		message.setError(HttpStatus.valueOf(HttpStatus.BAD_REQUEST.value()).getReasonPhrase());
-		message.setException(EmailAlreadyExistException.class.getName());
-		message.setMessage(ex.getMessage());
-		message.setUriDescription(request.getDescription(false));
-
-		return message;
 	}
 
 	@ExceptionHandler({ Exception.class })
-	@ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR)
-	public ErrorMessage globalExceptionHandler(Exception ex, WebRequest request) {
+	public ResponseEntity<Object> globalExceptionHandler(Exception ex, WebRequest request) {
 
-		ErrorMessage message = new ErrorMessage();
-
-		message.setTimestamp(new Date());
-		message.setStatusCode(HttpStatus.INTERNAL_SERVER_ERROR.value());
-		message.setError(HttpStatus.valueOf(HttpStatus.INTERNAL_SERVER_ERROR.value()).getReasonPhrase());
-		message.setException(ex.getClass().getCanonicalName());
-		message.setMessage(ex.getMessage());
-		message.setUriDescription(request.getDescription(false));
-
-		return message;
+		Map<String, Object> response = new HashMap<>();
+		response.put("timestamp", getCurrentTimestamp());
+		response.put("statusCode", HttpStatus.INTERNAL_SERVER_ERROR.value());
+		response.put("error", HttpStatus.valueOf(HttpStatus.INTERNAL_SERVER_ERROR.value()).getReasonPhrase());
+		response.put("exception", ex.getClass().getCanonicalName());
+		response.put("message", ex.getMessage());
+		response.put("uriDescription", request.getDescription(false));
+		return new ResponseEntity<>(response, new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
 	}
 }
