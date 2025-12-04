6812fc4015d49185121b92092323780d53652f29
shabtay shalem <shabtay.shalem@gmail.com>
2025-12-04 12:44:13 +0200

    04-12-2025 12:44

--- 02-JPA/delete/src/main/java/com/delete/service/UserService.java
+++ 02-JPA/delete/src/main/java/com/delete/service/UserService.java
@@ -1,6 +1,8 @@
 package com.delete.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 import com.delete.entity.UserEntity;
 import com.delete.repository.UserRepository;
 
@@ -8,21 +10,17 @@ import com.delete.repository.UserRepository;
 public class UserService {
 
 	public static int current = 1;
-	private final UserRepository userRepository;
 
-	public UserService(UserRepository userRepository) {
-		super();
-		this.userRepository = userRepository;
-	}
+	@Autowired
+	private UserRepository userRepository;
 
 	public UserEntity deleteAndCreateTest(UserEntity user) {
 		if (current > 2) {
 			userRepository.deleteById(1L);
-			throw new RuntimeException("Check if delete USer finsished or RollBack from DB performed");
+			throw new RuntimeException("Check if delete User finsished or RollBack from DB performed");
 		}
 		current++;
 		return userRepository.save(user);
-
 	}
 
 }
